2c2b9969af53799bafccd44d693dc127197be15f
scdanieli <23150094+scdanieli@users.noreply.github.com>
2023-09-21 12:43:10 +0200

    feat: init Liquidity Forecast report (DFL-2)

---
+++ liquidity_planning/liquidity_planning/report/liquidity_forecast/liquidity_forecast.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, ALYF GmbH and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Liquidity Forecast"] = {
+	"filters": [
+
+	]
+};
